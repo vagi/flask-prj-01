ea59b707210d925e9b91242af46d43b618199668
Vagif Molla <vagif69@gmail.com>
2021-06-23 19:21:20 +0300

    Create main page and add .get() method to avoid errors with empty args

--- app.py
+++ app.py
@@ -10,7 +10,15 @@ def get_index():
     return "<h1>Hello! You are on the Main page.</h1>"
 
 
+@app.route('/encrypt')
+def get_operation():
+    string = request.args.get('string', "You didn't enter your operation")
+    return f'Operation: {string}'
+
+
+
+
 # Inserting condition in case this file was used as not main application,
 # i.e imported as a module
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # When deployed on server to be changed to False
