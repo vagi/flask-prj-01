b1dd2ce5069cb1feb04b7b7da4beeafb0c0b1357
Vagif Molla <vagif69@gmail.com>
2021-06-25 01:37:11 +0300

    Implement check of empty input

--- app.py
+++ app.py
@@ -20,13 +20,16 @@ def encryption():
         return render_template('index.html',)
     else:
         # Getting input of text into the form
-        user_input = request.form.get('text', 'Input your text')
-        print("text:", user_input)
-        # Converting the text into bytes type
-        text_to_bytes = user_input.encode('utf-8')
-        # Encrypting the text
-        token = f.encrypt(text_to_bytes)
-        return f"<i>Encrypted result:</i> <h3>{token}</h3>"
+        user_input = request.form['text']
+        if user_input == '':
+            return "<i>Empty string entered!</i>"
+        else:
+            # Converting the text into bytes type
+            text_to_bytes = user_input.encode('utf-8')
+            # Encrypting the text
+            token = f.encrypt(text_to_bytes)
+            return f"<i>Encrypted result:</i><h3>{token}</h3>"
+
 
 # A decorator used to tell the application which URL is
 # associated with the following function
@@ -37,11 +40,14 @@ def decryption():
     else:
         # Getting input of token into the form
         token_ = request.form['text']
-        # Converting the string into bytes type
-        token_to_bytes = token_.encode('utf-8')
-        # Decrypting the token into text
-        output = f.decrypt(token_to_bytes)
-        return f'<i>Decrypted result:</i> <h3>{output.decode()}</h3>'
+        if token_ == '':
+            return "<i>Empty string entered!</i>"
+        else:
+            # Converting the string into bytes type
+            token_to_bytes = token_.encode('utf-8')
+            # Decrypting the token into text
+            output = f.decrypt(token_to_bytes)
+            return f'<i>Decrypted result:</i> <h3>{output.decode()}</h3>'
 
 
 # Inserting condition in case this file is used as a module (imported by another file)
