05727d85b35ce9c4cad9e1b2eff2ac56f199441a
Vagif Molla <vagif69@gmail.com>
2021-06-25 03:02:02 +0300

    Create new files app.args.py and index_args.html for text encryption/decryption by getting parameters from URL query string

---
+++ app_args.py
@@ -0,0 +1,50 @@
+# Import required libraries
+from flask import Flask, render_template, request
+from cryptography.fernet import Fernet
+
+# Initialize Flask object
+app = Flask(__name__)
+
+# Initialize Fernet object for encryption/decryption
+key = Fernet.generate_key()
+f = Fernet(key)
+
+# A decorator used to tell the application which URLs is
+# associated with the following function
+@app.route('/encrypt')
+def get_text():
+    # Getting input of text in URL's query string
+    string = request.args.get(
+        'string',
+        "You didn't enter your text",
+    )
+    # Converting the text into bytes type
+    text_to_bytes = string.encode('utf-8')
+    # Encrypting the text
+    token = f.encrypt(text_to_bytes)
+    # Removing prefix "b" with single quotes
+    token_ = token.decode()
+    return render_template('index_args.html', operation='Encrypted', token=token_)
+
+
+# A decorator used to tell the application which URL is
+# associated with the following function
+@app.route('/decrypt')
+def get_token():
+    # Getting input of token in URL's query string
+    token = request.args.get(
+        'string',
+        "You didn't enter your token",
+    )
+    # Converting the string into bytes type
+    token_to_bytes = token.encode('utf-8')
+    # Decrypting the token into text
+    output = f.decrypt(token_to_bytes)
+    # Removing prefix "b" with single quotes
+    output_ = output.decode()
+    return render_template('index_args.html', operation='Decrypted', token=output_)
+
+
+# Inserting condition in case this file is used as a module (imported by another file)
+if __name__ == '__main__':
+    app.run(debug=True)  # When deployed on server this to be changed to False
